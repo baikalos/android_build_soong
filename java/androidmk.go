51ac55585feadee9486f8740ebbbed1f8b2e93b8
Sergei Vasilev <svasiliev22@yandex.ru>
2025-11-27 20:08:21 +0300

    Baikalify 1
    Change-Id: I8429ad157ef91c60891d9f3ab6918124dff2df73

--- java/androidmk.go
+++ java/androidmk.go
@@ -304,7 +304,7 @@ func (app *AndroidApp) PrepareAndroidMKProviderInfo(config android.Config) *andr
 	info.PrimaryInfo.SetOptionalPath("LOCAL_SOONG_PROGUARD_DICT", app.dexer.proguardDictionary)
 	info.PrimaryInfo.SetOptionalPath("LOCAL_SOONG_PROGUARD_USAGE_ZIP", app.dexer.proguardUsageZip)
 
-	if app.Name() == "framework-res" || app.Name() == "org.lineageos.platform-res" {
+	if app.Name() == "framework-res" || app.Name() == "org.lineageos.platform-res" || app.Name() == "org.mineageos.platform-res" {
 		info.PrimaryInfo.SetString("LOCAL_MODULE_PATH", "$(TARGET_OUT_JAVA_LIBRARIES)")
 		// Make base_rules.mk not put framework-res in a subdirectory called
 		// framework_res.
@@ -409,7 +409,7 @@ func (a *AndroidLibrary) PrepareAndroidMKProviderInfo(config android.Config) *an
 			info.PrimaryInfo.SetPath("LOCAL_SOONG_AAR", a.aarFile)
 		}
 
-		if a.Name() == "framework-res" || a.Name() == "org.lineageos.platform-res" {
+		if a.Name() == "framework-res" || a.Name() == "org.lineageos.platform-res" || a.Name() == "org.mineageos.platform-res" {
 			info.PrimaryInfo.SetString("LOCAL_MODULE_PATH", "$(TARGET_OUT_JAVA_LIBRARIES)")
 			// Make base_rules.mk not put framework-res in a subdirectory called
 			// framework_res.
